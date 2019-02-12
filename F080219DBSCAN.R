9f48c109a7e17d1852c106f010cf03d81406f93f
felliks <fftampinongkol09@gmail.com>
2019-02-12 10:54:54 +0700

    Up

---
+++ F080219DBSCAN.R
@@ -0,0 +1,14 @@
+library(readxl)
+library(openxlsx)
+library(dplyr)
+library(dbscan)
+
+# Set path for load dataframe
+setwd('C:/Users/user/Dropbox/FORESTS2020/00AllData/Dataframe Sumatra/')
+dframe <- read_xlsx('DataWA_Line14_Sumatra.xlsx')
+dfx <- dframe %>% select(Band_2, Band_3, Band_4, Band_5, Band_6, Band_7)
+dfy <- dframe$frci_5m
+
+#----- DBSCAN 1
+plot(dfx$Band_4, dfy)
+clusDBSCAN <- dbscan(dfx, eps = 7, minPts = 10)
