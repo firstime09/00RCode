1e310813c29f3024a5c7a470f4dc6fc1171c5103
felliks <fftampinongkol09@gmail.com>
2019-03-06 18:29:02 +0700

    Up 06/03-2019

--- EarthLAB_040319.R
+++ EarthLAB_040319.R
@@ -5,6 +5,7 @@ library(rgeos)
 options(stringsAsFactors = FALSE) #--- Turn off factors
 #--- Load the landsat bands in our computer
 setwd('C:/Users/user/Dropbox/FORESTS2020/00AllData/DATA/From Eci/landsat/LC80340322016189-SC20170128091153/crop')
+#setwd('D:/00AllData/00 Data Load/Path112Row56 Manado')
 all_landsat_bands <- list.files(pattern = glob2rx("*band*.tif$"), full.names = TRUE)
 
 #--- Creat spatial raster stack from the list of file names
@@ -30,3 +31,41 @@ par(xpd = FALSE, mar = c(0,0,1,5))
 cloud_mask_189[cloud_mask_189 > 0] <- NA
 plot(cloud_mask_189, main = "The Raster Mask", col = c("green"),
      legend = FALSE, axes = FALSE, box = FALSE)
+
+par(xpd = TRUE) # force legend to plot outside of the plot extent
+legend(x = cloud_mask_189@extent@xmax, cloud_mask_189@extent@ymax,
+       c("Not masked", "Masked"),
+       fill = c("green", "white"),
+       bty = "n")
+
+# mask the stack
+all_landsat_bands_mask <- mask(all_landsat_bands_br, mask = cloud_mask_189)
+
+# check the memory situation
+inMemory(all_landsat_bands_mask)
+## [1] TRUE
+class(all_landsat_bands_mask)
+## [1] "RasterBrick"
+## attr(,"package")
+## [1] "raster"
+
+# plot RGB image
+# first turn all axes to the color white and turn off ticks
+par(col.axis = "white", col.lab = "white", tck = 0)
+# then plot the data
+plotRGB(all_landsat_bands_mask,
+        r = 4, g = 3, b = 2,
+        main = "Landsat RGB Image \n Are the clouds gone?",
+        axes = TRUE)
+box(col = "white")
+
+# plot RGB image
+# first turn all axes to the color white and turn off ticks
+par(col.axis = "white", col.lab = "white", tck = 0)
+# then plot the data
+plotRGB(all_landsat_bands_mask,
+        r = 4, g = 3, b = 2,
+        stretch = "lin",
+        main = "Landsat RGB Image \n Are the clouds gone?",
+        axes = TRUE)
+box(col = "white")
