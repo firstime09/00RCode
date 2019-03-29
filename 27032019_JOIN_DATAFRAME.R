3e0a50a9347f771349b8069d48709bf97b298e7e
felliks <fftampinongkol09@gmail.com>
2019-03-29 18:48:26 +0700

    UP 29/03-2019

--- 27032019_JOIN_DATAFRAME.R
+++ 27032019_JOIN_DATAFRAME.R
@@ -2,26 +2,36 @@ library(openxlsx)
 library(dplyr)
 library(readxl)
 
-setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
-file1 <- read.csv('Cidanau_NN_130319.csv')
-head(file1)
-delColumn <- file1[-c(1,9)]
+# setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
+# file1 <- read.csv('Cidanau_NN_130319.csv')
+# head(file1)
+# delColumn <- file1[-c(1,9)]
+# write.xlsx(delColumn, file = 'Cidanau_NN_130319.xlsx')
 
-write.xlsx(delColumn, file = 'Cidanau_NN_130319.xlsx')
-
-#---- Gabung DataFrame
-setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
-df1 <- read_xlsx('Cidanau_Join_LINE6.xlsx')
+##---- Gabung DataFrame
+# Load first data.frame
+setwd('F:/All Data Forests2020/29032019_Pavilion/DATA NO_NORMALIZE/')
+df1 <- read_xlsx('CIDANAU_29032019_68.16.xlsx')
 head(df1)
-df2 <- read_xlsx('FRCI_LINE7_78.13.xlsx')
+
+# Load second data.frame
+setwd('F:/All Data Forests2020/29032019_Pavilion/DATA NO_NORMALIZE/DATA SUMATERA/Malta_LINE_1_2/')
+df2 <- read_xlsx('FRCI_LINE1_74.15.xlsx')
 head(df2)
-delcoll <- df2[-8]
-head(delcoll)
+Change_position <- df2[c(7,1,2,3,4,5,6,8)]
+df2 <- Change_position
+head(df2)
 
-join_df <- full_join(df1, delcoll)
+# delcoll <- df2[-8]
+# head(delcoll)
+join_df <- full_join(df1, df2)
 View(join_df)
-write.xlsx(join_df, file = 'Cidanau_Join_LINE6_7.xlsx')
 
-#---- Plot data CIDANAU 25/03-2019
-setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
-loaDF <- read_excel('Cidanau_Join_LINE6_61.18.xlsx')
+# Saved NEW data.frame
+setwd('F:/All Data Forests2020/00ResultDataFrame/JOIN DATA_FRAME/')
+write.xlsx(join_df, file = 'CIDANAU_LINE_1_SUMATERA.xlsx')
+
+
+# #---- Plot data CIDANAU 25/03-2019
+# setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
+# loaDF <- read_excel('Cidanau_Join_LINE6_61.18.xlsx')
--- DBSCAN_I.R
+++ DBSCAN_I.R
@@ -9,23 +9,22 @@ library(dismo)
 library(openxlsx)
 
 # setwd("C:/Users/user/Dropbox/FORESTS2020/00AllData/Dataframe Sumatra/Data FRCI Window Area_Malta/")
-setwd("D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/")
-file = read_excel("FRCI_Line_7.xlsx")
+setwd("C:/Users/Felix/Dropbox/FORESTS2020/00AllData/")
+file = read_excel("Data_1048_Yoga.xlsx")
 # file =read.csv("FRCI_Line_6.csv")
 head(file)
-dataall <- file[,-c(3,10)] ## Drop column Band_1 and Band_9 in dataframe
-data<-file[,-c(3,10)] ## Drop column Band_1 and Band_9 in dataframe
+dataall <- file[,-c(1,2,4,6,13)] ## Drop column Band_1 and Band_9 in dataframe
+data<-file[,-c(1,2,4,6,13)] ## Drop column Band_1 and Band_9 in dataframe
 head(data)
 
-number <-data %>%
+number <- data %>%
   group_by(Class) %>%
   summarize(n())
-sample <-data%>%
+sample <- data%>%
   group_by(Class)%>%
   sample_n(min(number$`n()`))
 head(sample)
-sample<-sample[-2] ## For remove column Class
-
+sample <- sample[-1] ## For remove column Class
 
 head(sample)
 # lst <- as.data.frame(lapply(sample, function(x) round((x-min(x))/(max(x)-min(x)), 3))) 
@@ -34,33 +33,33 @@ head(lst)
 dataSample <- lst
 head(dataSample)
 kNNdistplot(dataSample, k = 5)
-abline(h=0.03, col = "red", lty=2) #----------------- Note
+abline(h=0.055, col = "red", lty=2) #----------------- Note
 
-res <- dbscan(dataSample, eps =0.03 , minPts = 5)
+res <- dbscan(dataSample, eps = 0.055 , minPts = 5)
 res
 pairs(dataSample, col = res$cluster + 1L)
-dataSample$cluster<-res$cluster
-cleanall<-dataSample %>% filter(cluster > 0)
+dataSample$cluster <- res$cluster
+cleanall <- dataSample %>% filter(cluster > 0)
 par(mfrow=c(1,2))
 plot(cleanall$Band_4, cleanall$frci)
 plot(dataSample$Band_4, dataSample$frci)
 
-# setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/') #---------------------- After running
-# write.xlsx(cleanall, file = "FRCI_LINE7_78.13.xlsx")
+# setwd('F:/All Data Forests2020/29032019_Pavilion/CIDANAU NO_NORMALIZE/') #---------------------- After running
+# write.xlsx(cleanall, file = "CIDANAU_29032019_68.16.xlsx")
 # write.csv(cleanall, file = "FRCI_LINE7_78.13.csv")
+
 ## Feature Selection
 svrdata <- cleanall
 svrdata <- cleanall[-8]
 head(svrdata)
-#library(Boruta)
+
 # Decide if a variable is important or not using Boruta
 boruta_output <- Boruta(frci ~ ., data=na.omit(svrdata), doTrace=2)  # perform Boruta search
 boruta_signif <- names(boruta_output$finalDecision[boruta_output$finalDecision %in% ("Confirmed")])  # collect Confirmed and Tentative variables
 print(boruta_signif)  # significant variables
 plot(boruta_output, cex.axis=.7, las=2, xlab="", main="Variable Importance")  # plot variable importance
 
-# Divide data to training and testing ===============================
-
+# Divide data to training and testing ================================
 set.seed(3033)
 intrain <- createDataPartition(y = svrdata$frci, p= 0.7, list = FALSE)
 training <- svrdata[intrain,]
@@ -69,21 +68,19 @@ testing <- svrdata[-intrain,]
 dim(training)
 dim(testing)
 anyNA(svrdata)
+
 ## RMSE
 rmse <- function(error)
 {
   sqrt(mean(error^2))
 }
 
-# svr model ==============================================
-
+# svr model =========================================================
 model <- svm(frci ~ . , training)
 predictedY <- predict(model, testing)
 
-
 error <- testing$frci - predictedY  # 
 svrPredictionRMSE <- rmse(error)  #  
-
 
 tuneResult <- tune(svm, frci ~ .,  data = training,
                    ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
@@ -96,25 +93,18 @@ tuneResult <- tune(svm, frci ~ .,  data = training,
 tunedModel <- tuneResult$best.model
 tunedModelY <- predict(tunedModel, testing)
 error <- testing$frci - tunedModelY
-tunedModelRMSE <- rmse(error)  # 2.219642
+tunedModelRMSE <- rmse(error)
 
 # 1. 'Actual' and 'Predicted' data
 df <- data.frame(testing$frci, tunedModelY)
-
 # 2. R2 Score components
-
 # 2.1. Average of actual data
 avr_y_actual <- mean(df$testing.frci)
-
 # 2.2. Total sum of squares
 ss_total <- sum((df$testing.frci - avr_y_actual)^2)
-
 # 2.3. Regression sum of squares
 ss_regression <- sum((df$tunedModelY - avr_y_actual)^2)
-
 # 2.4. Residual sum of squares
 ss_residuals <- sum((df$testing.frci - df$tunedModelY)^2)
-
 # 3. R2 Score
 r2 <- 1 - ss_residuals / ss_total
-
