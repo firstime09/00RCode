8f42fc24beec2011a2abe68a705124818e2e3bff
felliks <fftampinongkol09@gmail.com>
2019-04-01 14:40:37 +0700

    UP Data

--- 27032019_JOIN_DATAFRAME.R
+++ 27032019_JOIN_DATAFRAME.R
@@ -2,14 +2,14 @@ library(openxlsx)
 library(dplyr)
 library(readxl)
 
-setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
-file1 <- read.csv('Cidanau_NN_130319.csv')
-head(file1)
-delColumn <- file1[-c(1,9)]
+# setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
+# file1 <- read.csv('Cidanau_NN_130319.csv')
+# head(file1)
+# delColumn <- file1[-c(1,9)]
+# 
+# write.xlsx(delColumn, file = 'Cidanau_NN_130319.xlsx')
 
-write.xlsx(delColumn, file = 'Cidanau_NN_130319.xlsx')
-
-#---- Gabung DataFrame
+##---- Gabung DataFrame
 setwd('D:/00RCode/Result/Data Sumatera/Data Sumatera No_Normalize/')
 df1 <- read_xlsx('Cidanau_NN_130319.xlsx')
 head(df1)
